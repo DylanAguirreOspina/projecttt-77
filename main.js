002dd3695d0568065b115609ba3d1ae589bf8047
DylanAguirreOspina <85912090+DylanAguirreOspina@users.noreply.github.com>
2022-01-06 20:32:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstname="dylan";
+var lastname="Aguirre";
+console.log(firstname.concat(lastname));
+var a= 10;
+var b=5;
+console.log(Math.sqrt(a));
+console.log(Math.max(a,b));
+console.log(Math.min(a,b));
+console.log(Math.pow(a,b));
